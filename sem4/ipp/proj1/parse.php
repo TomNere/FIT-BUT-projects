1371999cbbb4e56043c725647f501351a37a4003
tondo8881 <xnerec00@vutbr.cz>
2018-02-09 17:52:40 +0100

    Add first line check

--- sem4/ipp/proj1/parse.php
+++ sem4/ipp/proj1/parse.php
@@ -1,19 +1,26 @@
 <?php 
 
+// Arguments check
 const ARG_ERR = 10;
-inst_counter = 0;
+$inst_counter = 0;
 
 if ($argc > 1) {
     if ($argv[1] == '--help') {
         print "show passed\n";
     }
     else {
-        fwrite(STDERR, "Neznamy argument.\n");
+        fwrite(STDERR, "Unknown arguments!\n");
         return 10;
     }
 }
-else {
-    print "no argument passed\n";
+
+// First line check
+$first_line = rtrim(strtolower(fgets(STDIN)));
+if (strcmp($first_line, ".ippcode18") != 0) {
+    fwrite(STDERR, ".IPPcode18 is missing!\n");
+    return 21;
 }
+
+//string getInst()
 
 ?>
