3ef4f77307ef117bbc4145ad9fb14a1fc40fe25c
tondo8881 <xnerec00@vutbr.cz>
2018-02-10 18:48:19 +0100

    Add arrays of instructions and simple getInst function

--- sem4/ipp/proj1/parse.php
+++ sem4/ipp/proj1/parse.php
@@ -1,11 +1,22 @@
 <?php 
-/*********************FUNCTION DEFS************************/
+/*********************CLASS DEFS***************************/
 
-function getInst() {
-    return strtolower(stream_get_line(STDIN, 20, " "));
+/**
+* This class represents instruction
+*/
+class Instruction {
+    public $name;
+    public $params;
 }
 
 /********************ARRAYS FOR INSTRUCTIONS*****************/
+
+$help_arr = array(
+    "inst_0" => 0,
+    "inst_1" => 1,
+    "inst_2" => 2,
+    "inst_3" => 3
+);
 
 // Zero parameter instructions
 $inst_0 = array("createframe", "pushframe", "popframe", "return", "break");
@@ -14,7 +25,30 @@ $inst_0 = array("createframe", "pushframe", "popframe", "return", "break");
 $inst_1 = array("defvar", "call", "pushs", "pops", "write", "label", "jump", "dprint");
 
 // Two parameter instructions
-$inst_2 = array()
+$inst_2 = array("move", "int2char", "read", "strlen", "type");
+
+// Three parameter instructions
+$inst_3 = array("add", "sub", "mul", "idiv", "lt", "gt", "eq", "and", "or", "not", "stri2int", "concat", "getchar", "setchar", "jumpifeq", "jumpifneq");
+
+/*********************FUNCTION DEFS************************/
+
+function getInst() {
+    global $help_arr, $inst_0, $inst_1, $inst_2, $inst_3;
+
+    $inst = new Instruction();
+    $inst->name = strtolower(stream_get_line(STDIN, 20, " "));
+
+    foreach ($help_arr as $arr_name => $number) {
+        foreach ($$arr_name as $value) {
+            if ($value == $inst->name) {
+                $inst->params = $number;
+                return $inst;
+            }
+        }
+    }
+    $inst->params = -1;
+    return $inst;
+}
 
 // Arguments check
 const ARG_ERR = 10;
@@ -37,9 +71,8 @@ if (strcmp($first_line, ".ippcode18") != 0) {
     return 21;
 }
 
-print getInst();
-print "\n";
-print fgets(STDIN);
-
+$var = getInst();
+print $var->name;
+print $var->params;
 
 ?>
