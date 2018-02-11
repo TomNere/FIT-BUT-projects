f9e9a359d6a700f329a4268afc16a3ffea6dea94
tondo8881 <xnerec00@vutbr.cz>
2018-02-11 23:21:46 +0100

    Add all_inst array

--- sem4/ipp/proj1/parse.php
+++ sem4/ipp/proj1/parse.php
@@ -9,6 +9,8 @@ class Instruction {
     public $params;
 }
 
+const ARG_ERR = 10;
+
 /********************ARRAYS FOR INSTRUCTIONS*****************/
 
 $help_arr = array(
@@ -16,6 +18,56 @@ $help_arr = array(
     "inst_1" => 1,
     "inst_2" => 2,
     "inst_3" => 3
+);
+
+/**
+* All instructions 
+* KEY - operation code
+* VALUE - array of parameters
+* 1 - variable
+* 2 - label
+* 3 - symb
+* 4 - type
+* 5 -
+*/
+$all_inst = array(
+    "createframe" => array (), 
+    "pushframe" => array (), 
+    "popframe" => array (), 
+    "return" => array (), 
+    "break" => array (),
+
+    "defvar" => array (1), 
+    "call" => array (2), 
+    "pushs" => array (3), 
+    "pops" => array (1), 
+    "write" => array (3), 
+    "label" => array (2), 
+    "jump" => array (2), 
+    "dprint" => array (3),
+
+    "move" => array (1, 3), 
+    "int2char" => array (1, 3), 
+    "read" => array (1, 4), 
+    "strlen" => array (1, 3), 
+    "type" => array (1, 3),
+
+    "add" => array(1, 3, 3), 
+    "sub" => array(1, 3, 3), 
+    "mul" => array(1, 3, 3), 
+    "idiv" => array(1, 3, 3), 
+    "lt" => array(1, 3, 3), 
+    "gt" => array(1, 3, 3), 
+    "eq" => array(1, 3, 3), 
+    "and" => array(1, 3, 3), 
+    "or" => array(1, 3, 3), 
+    "not" => array(1, 3, 3), 
+    "stri2int" => array(1, 3, 3), 
+    "concat" => array(1, 3, 3), 
+    "getchar" => array(1, 3, 3), 
+    "setchar" => array(1, 3, 3), 
+    "jumpifeq" => array(2, 3, 3), 
+    "jumpifneq" => array(2, 3, 3)
 );
 
 // Zero parameter instructions
@@ -50,29 +102,42 @@ function getInst() {
     return $inst;
 }
 
-// Arguments check
-const ARG_ERR = 10;
-$inst_counter = 0;
+function main() {
 
-if ($argc > 1) {
-    if ($argv[1] == '--help') {
-        print "show passed\n";
+    // Arguments check
+    global $argc;
+    global $argv;
+
+    $inst_counter = 0;
+    
+    if ($argc > 1) {
+        if ($argv[1] == '--help') {
+            print "show passed\n";
+        }
+        else {
+            fwrite(STDERR, "Unknown arguments!\n");
+            return 10;
+        }
     }
-    else {
-        fwrite(STDERR, "Unknown arguments!\n");
-        return 10;
+    
+    // First line check
+    $first_line = rtrim(strtolower(fgets(STDIN)));
+    if (strcmp($first_line, ".ippcode18") != 0) {
+        fwrite(STDERR, ".IPPcode18 is missing!\n");
+        return 21;
     }
+    
+    // Repeat until error or EOF
+    while ( true ) {
+        $var = getInst();
+        //print $var->name;
+        //print $var->params;
+    }
+    
+
+    return 0;
 }
 
-// First line check
-$first_line = rtrim(strtolower(fgets(STDIN)));
-if (strcmp($first_line, ".ippcode18") != 0) {
-    fwrite(STDERR, ".IPPcode18 is missing!\n");
-    return 21;
-}
-
-$var = getInst();
-print $var->name;
-print $var->params;
+return main();
 
 ?>
