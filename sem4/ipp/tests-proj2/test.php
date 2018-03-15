09631e1166fa3511fe4271dfb405d7fb761383f8
tondo8881 <xnerec00@vutbr.cz>
2018-03-15 12:19:20 +0100

    IPP tests for 1.delivery

--- sem4/ipp/tests-proj2/test.php
+++ sem4/ipp/tests-proj2/test.php
@@ -16,7 +16,7 @@ const ARG_MESS = "Nesprávná kombinace argument, try --help.";
 /************************GLOBAL VARIABLES************************/
 
 $parse_file = "./parse.php";
-$int_file = "./interpret.php";
+$int_file = "./interpret.py";
 $directory = "./";
 $recursive = false;
 $dir_array = Array ();
@@ -85,8 +85,9 @@ function getDirs() {
     $rec_iter = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory)); 
 
     foreach ($rec_iter as $file) {
-        if ($file->isDir())
-        array_push($dir_array, $file->getPathname());
+        // Check for duplicates
+        if ($file->isDir() && !in_array(substr($file, 0, strlen($file) - 1), $dir_array) )
+            array_push($dir_array, $file->getPathname());
     }
 }
 
@@ -94,12 +95,138 @@ function getDirs() {
 /*************************************MAIN FUNCTION**************************************/
 
 argHandle();                            // Parse arguments
-array_push($dir_array, $directory);     // Add first directory to array of all directories
 
-if ($recursive) {                        // Get array of all subdirectories
+if ($recursive) {                       // Get array of all subdirectories
     getDirs();
 }
+else {
+    array_push($dir_array, $directory.'/.'); // Only 1 directory
+}
 
-var_dump($dir_array);
+// HTML
+print(
+"<!DOCTYPE html>\n
+    <html lang=\"cs\">\n
+    <head>\n
+        <meta charset=\"utf-8\">\n
+        <title>IPP-prehled testu</title>\n
+    </head>\n
+    <body style=\"height:100%;font-family: 'arial'; margin: 0px;padding: 0px;background-color: LightCyan;\">\n
+        <div style=\"text-align:center;vertical-align: middle;line-height: 61px;height: 61px;background-color: rgba(20, 61, 102, 1);color: white;\";><h1 style=\"margin-top: 0;\">Přehled úspěšnosti testů pro parse.php a interpret.py</h1></div>\n"
+);
+
+$test_count = 0;
+$success_count = 0;
+
+foreach ($dir_array as $key_d => $value_d) {
+    $file_array = scandir($value_d);
+
+    // Skip directories without test files
+    $something = preg_grep ('/^.*\.(src)$/i', $file_array);
+    if (empty($something))
+        continue;
+
+    print("<div style=\"background-color:PowderBlue;\">\n<h1 style=\"margin-top: 0;color:navy;\">Testy z adresáře:&emsp;&emsp;".$value_d."</h2>\n<ol style=\"font-size: 18pt;\">\n");
+
+    $dir_success = 0;
+    $dir_count = 0;
+    foreach ($file_array as $key_f => $value_f) {
+        // Find .src file
+        if (!strcmp(substr($value_f, strpos($value_f, '.') + 1), 'src')) {
+            $test_count++;
+            $dir_count++;
+
+            $file_name = substr($value_f, 0, strpos($value_f, '.'));
+
+            // Remove last . from directory
+            $actual_dir = substr($value_d, 0, strlen($value_d) - 1);
+
+            if (!in_array($file_name.'.rc', $file_array)) {
+                // Create rc file with 0
+                file_put_contents($actual_dir.$file_name.'.rc', "0\n");
+            }
+            if (!in_array($file_name.'.in', $file_array)) {
+                // Create rc file with 0
+                file_put_contents($actual_dir.$file_name.'.in', "");
+            }
+            if (!in_array($file_name.'.out', $file_array)) {
+                // Create rc file with 0
+                file_put_contents($actual_dir.$file_name.'.out', "");
+            }
+
+            $returned_code;
+            $output;
+
+            // Exec command
+            $command = 'php56 '.$parse_file.' < '.$actual_dir.$file_name.'.src'.' 2> /dev/null';
+
+            exec($command, $output, $returned_code);
+
+            // If error in parse.php occured
+            if ($returned_code != 0) {
+                $rc_file = fopen($actual_dir.$file_name.'.rc', 'r');
+                $rc = fread($rc_file, 2);
+                fclose($rc_file);
+
+                if (!strcmp($rc, $returned_code)) {
+                    $dir_success++;
+                    $success_count++;
+                    print("<li> Testovací soubor: ".$file_name." - <span style=\"font-size: 22pt;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;color: green;font-weight: bold;\">Úspěch</span> </li>");
+                }
+                else
+                    print("<li> Testovací soubor: ".$file_name." - <span style=\"font-size: 22pt;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;color: red;font-weight: bold;\">Neúspěch</span>&emsp;&hArr; Rozdílný návratový kód od parse.php</li>");
+            }
+            else {
+                // Create file for XML
+                $tmp_f = tmpfile();
+                $tmp_f_uri = stream_get_meta_data($tmp_f);
+                file_put_contents($tmp_f_uri['uri'], $output);
+
+                $command = "python3.6 ".$int_file." --source=".$tmp_f_uri['uri'].' < '.$actual_dir.$file_name.'.in'.' 2> /dev/null';
+
+                unset($output);
+                exec($command, $output, $returned_code);
+
+                // If error in interpret.py occured
+                if ($returned_code != 0) {
+                    $rc_file = fopen($actual_dir.$file_name.'.rc', 'r');
+                    $rc = fread($rc_file, 2);
+                    fclose($rc_file);
+
+                    if (!strcmp($rc, $returned_code)) {
+                        $dir_success++;
+                        $success_count++;
+                        print("<li> Testovací soubor: ".$file_name." - <span style=\"font-size: 22pt;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;color: green;font-weight: bold;\">Úspěch</span> </li>");
+                    }
+                    else
+                        print("<li> Testovací soubor: ".$file_name." - <span style=\"font-size: 22pt;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;color: red;font-weight: bold;\">Neúspěch</span>&emsp;&hArr; Rozdílný návratový kód od interpret.py</li>");
+                }
+                // Check for diff
+                else {
+                    file_put_contents($tmp_f_uri['uri'], $output);
+                    unset($output);
+                    $command = "diff ".$actual_dir.$file_name.'.out '.$tmp_f_uri['uri'];
+                    exec($command, $output, $returned_code);
+
+                    if (!isset($output[0])) {
+                        $dir_success++;
+                        $success_count++;
+                        print("<li> Testovací soubor: ".$file_name." - <span style=\"font-size: 22pt;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;color: green;font-weight: bold;\">Úspěch</span> </li>");
+                    }
+                    else
+                        print("<li> Testovací soubor: ".$file_name." - <span style=\"font-size: 22pt;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;color: red;font-weight: bold;\">Neúspěch</span>&emsp;&hArr; Rozdílný výstup od interpret.py</li>");
+                }
+                fclose($tmp_f);
+            }
+        }
+    }
+
+    print("</ol>\n<hr>
+        </div>\n
+        <div style=\"height: 40px;font-size: 22pt;font-weight: bold;\">&emsp;&emsp;Úspěšnost testů z daného adresáře: ".$dir_success."/".$dir_count.".</div><hr style=\"padding: 0px; margin: -1px; \">"); 
+}
+
+print("<div style=\"border:5px solid black;height:81px;line-height:81px;vertical-align: middle;background-color: PaleGreen;text-align: center;font-size: 30pt;font-weight: bold;\"> Celková úspěšnost všech testů: ".$success_count."/".$test_count.".</div>
+        </body>\n</html>");
 
 ?>
