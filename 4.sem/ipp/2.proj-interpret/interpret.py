addff9e10a34b6974d0435772ab9cba60f39c469
tondo8881 <xnerec00@vutbr.cz>
2018-03-21 01:23:09 +0100

    Switch is back

--- 4.sem/ipp/2.proj-interpret/interpret.py
+++ 4.sem/ipp/2.proj-interpret/interpret.py
@@ -26,6 +26,7 @@ def retError(err, msg):
     exit(err)
 
 def getFrame(frame):
+    global global_frame
     if frame == 'GF':
         return global_frame
     elif frame == 'LF':
@@ -33,20 +34,30 @@ def getFrame(frame):
     elif frame == 'TF':
         pass
 
+
 # Translate variable if variable, else return constant
 def varTranslate(var):
     if var['type'] == 'bool' or var['type'] == 'int' or var['type'] == 'string':
         return var
     elif var['type'] == 'var':
-        frame = getFrame(var['text'][:2])
-        if var['text'][3:] in frame:
-            return frame[var['text'][3:]]
+        frame = getFrame(var['value'][:2])
+        if var['value'][3:] in frame:
+            return frame[var['value'][3:]]
         else:
             retError(VAR_ERR, "Undefined variable")
     else:
         retError(SYN_ERR, "Unknown type of symbol")
 
+# Check number of arguments
+# TODO
+def argCheck(inst, number):
+    pass
+
 """""""""""""""""""""""""""""""""""""HELPING_FUNCTIONS"""""""""""""""""""""""""""""""""""""""""""
+
+# Nothing to do
+def inothing(inst):
+    pass
 
 def icreateframe():
     return ""
@@ -64,7 +75,7 @@ def ibreak():
     return ""
 
 def idefvar(inst):
-    name = inst['args'][0]['text']
+    name = inst['args'][0]['value']
     frame = getFrame(name[:2])
     name = name[3:]
     frame.update({name: {'type': None, 'value': None}})
@@ -72,31 +83,57 @@ def idefvar(inst):
 def icall():
     return ""
 
-def ipushs():
-    return ""
+# Push value on the top of data stack
+def ipushs(inst):
+    if len(inst['args']) == 1:
+        var = varTranslate(inst['args'][0])
+        data_stack.append(var)
+    else:
+        retError(ARG_ERR, ARG_STR)
 
-def ipops():
-    return ""
 
-def iwrite():
-    return ""
+def ipops(inst):
+    if len(inst['args']) == 1:
+        frame = getFrame(inst['args'][0]['value'][:2])
+        name = inst['args'][0]['value'][3:]
 
-def ilabel(inst):
-    return
+        if name in frame:                           # Defined
+            frame[name] = data_stack[-1]
+            del data_stack[-1]
+        else:
+            retError(VAR_ERR, "Undeclared Variable")
+    else:
+        retError(ARG_ERR, ARG_STR)
 
-def ijump():
-    return ""
+
+def iwrite(inst):
+    if len(inst['args']) == 1:
+        var = varTranslate(inst['args'][0])
+        print (var['value'])
+    else:
+        retError(SYN_ERR, ARG_STR)
+
+
+def ijump(inst):
+    if len(inst['args']) == 1:
+        if inst['args'][0]['value'] in label_arr:
+            allInst.setCounter(label_arr[inst['args'][0]['value']])
+        else:
+            retError(SEM_ERR, 'Undefined label')
+    else:
+        retError(SYN_ERR, ARG_STR)
+
 
 def idprint():
     return ""
 
 def imove(inst):
     if inst['args'][1]['type'] != 'var':        # Constant
-        frame = getFrame(inst['args'][0]['text'][:2])
-        if inst['args'][0]['text'][3:] in frame:
-            frame.update({inst['args'][0]['text'][3:]: {'type': inst['args'][1]['type'], 'value': inst['args'][1]['text']}})
+        frame = getFrame(inst['args'][0]['value'][:2])
+        if inst['args'][0]['value'][3:] in frame:
+            frame.update({inst['args'][0]['value'][3:]: {'type': inst['args'][1]['type'], 'value': inst['args'][1]['value']}})
         else:
-            exit(31)
+            retError(VAR_ERR, "Undefined variable")
 
 def iint2char():
     return ""
@@ -152,7 +189,7 @@ def igetchar():
 def isetchar():
     return ""
 
-def ijumpifeq(inst):
+def ijumpif(inst):
     if len(inst['args']) != 3:
         retError(SYN_ERR, ARG_STR)
 
@@ -162,16 +199,59 @@ def ijumpifeq(inst):
     if var1['type'] != var2['type']:
         retError(TYPE_ERR, "Type incompatibility")
 
-    if inst['args'][0]['text'] in label_arr:
-        if var1 == var2:                                                # Jump
-            allInst.setCounter(label_arr[inst['args'][0]['text']])
-        else:                                                           # Nothing to do
-            pass
+    if inst['args'][0]['value'] in label_arr:
+        if inst['opcode'] == 'JUMPIFEQ':                                    # JUMPIFEQ
+            if var1 == var2:
+                allInst.setCounter(label_arr[inst['args'][0]['value']])
+        else:                                                               # JUMPIFNEQ
+            if var1 != var2:
+                allInst.setCounter(label_arr[inst['args'][0]['value']])
     else:
         retError(SEM_ERR, "Undefined label")
 
-def ijumpifneq():
-    return ""
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""MAIN_SWITCH"""""""""""""""""""""""""""""""""""""""""""""""""""
+
+def mainSwitch(inst):
+    switcher = {
+        'CREATEFRAME': icreateframe,
+        'PUSHFRAME': ipushframe,
+        'POPFRAME': ipopframe,
+        'RETURN': ireturn,
+        'BREAK': ibreak,
+        'DEFVAR': idefvar,
+        'CALL': icall,
+        'PUSHS': ipushs,
+        'POPS': ipops,
+        'WRITE': iwrite,
+        'LABEL': inothing,
+        'JUMP': ijump,
+        'DPRINT': idprint,
+        'MOVE': imove,
+        'INT2CHAR': iint2char,
+        'READ': iread,
+        'STRLEN': istrlen,
+        'TYPE': itype,
+        'ADD': iadd,
+        'SUB': isub,
+        'MUL': imul,
+        'IDIV': iidiv,
+        'LT': ilt,
+        'GT': igt,
+        'EQ': ieq,
+        'AND': iand,
+        'OR': ior,
+        'NOT': inot,
+        'STRI2INT': istri2int,
+        'CONCAT': iconcat,
+        'GETCHAR': igetchar,
+        'SETCHAR': isetchar,
+        'JUMPIFEQ': ijumpif,
+        'JUMPIFNEQ': ijumpif,
+    }
+
+    func = switcher.get(inst, lambda: "Unknown")
+    func(allInst.getInst())
 
 """ Class representing array of all instructions
 #   Include program counter
@@ -208,9 +288,11 @@ class InstArr:
         pprint(self.instructions)
 
 
-def createArr(arr, labels, path):
+def createArr(path):
     tree = ET.parse(path)
     root = tree.getroot()
+    global label_arr
+    global allInst
 
     for child in root:
         if child.tag == 'instruction':              # If instruction, add to array
@@ -219,19 +301,19 @@ def createArr(arr, labels, path):
             for arg in child:                   # Iterate in arguments
                 arg = {
                     'type': arg.attrib['type'],
-                    'text': arg.text
+                    'value': arg.text
                 }
                 args.append(arg)
-            arr.addInst(child.attrib['opcode'], args)       # Add instruction with args to array of all instructions
+            allInst.addInst(child.attrib['opcode'], args)       # Add instruction with args to array of all instructions
 
             if child.attrib['opcode'] == 'LABEL':   # If label, add to array of all labels
                 if args[0]['type'] != 'label' or len(args) != 1:
                     retError(SYN_ERR, ARG_STR)
 
-                if args[0]['text'] in label_arr:  # Redefinition of label
+                if args[0]['value'] in label_arr:  # Redefinition of label
                     retError(SEM_ERR, "Redefinition of label.")
 
-                label_arr.update({args[0]['text']: child.attrib['order']})
+                label_arr.update({args[0]['value']: child.attrib['order']})
 
         elif child.tag != 'name' and child.tag != 'description':  # Unknown element
             exit(31)
@@ -241,7 +323,8 @@ def createArr(arr, labels, path):
 label_arr = {}          # Array of all labels
 allInst = InstArr()
 global_frame = {}       # Array of variables in global frame
-frame_stack = {}        # Stack of frames
+frame_stack = []        # Stack of frames
+data_stack = []         # Stack of variables
 
 
 def main():
@@ -253,14 +336,17 @@ def main():
 
     path = arguments.source[0]
 
-    global label_arr
     global allInst
-    createArr(allInst, label_arr, path)
+    createArr(path)
 
     # Iterate in all instructions
     while allInst.inst_counter < allInst.count:
         allInst.incCounter()
-        globals()['i' + allInst.getInst()['opcode'].lower()](allInst.getInst())         # Call function assigned to opcode
+        mainSwitch(allInst.getInst()['opcode'])
+        #print('frame')
+        #pprint(global_frame)
+        #print('stack')
+        #pprint(data_stack)
 
 
 if __name__ == "__main__":
