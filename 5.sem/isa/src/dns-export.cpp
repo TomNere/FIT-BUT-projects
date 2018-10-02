6214a097f3042ed7c0323b80da7506eb6e9772e9
tondo8881 <xnerec00@vutbr.cz>
2018-10-02 22:01:10 +0200

    Logging methods added

--- 5.sem/isa/src/dns-export.cpp
+++ 5.sem/isa/src/dns-export.cpp
@@ -1,6 +1,7 @@
-#include <iostream>		// no comment needed...
-#include <unistd.h>		// getopt
+#include <iostream>		// No comment needed...
+#include <unistd.h>		// Getopt
 #include <signal.h>     // Signal shandling
+#include <pcap.h>		// No comment needed...
 
 using namespace std;
 
@@ -14,13 +15,13 @@ const string help = "Invalid parameters!\n\n"
 /*****************************************************************************************************************************/
 
 // Structure representing parameters of running program in raw string format
-struct raw_parameters
+typedef struct raw_parameters
 {
 	string file;
 	string interface;
 	string syslogServer;
 	string time;
-};
+} raw_parameters;
 
 /********************************************************** METHODS **********************************************************/
 
@@ -39,52 +40,98 @@ void handleSig(int sigNum)
 
 
 // Argument parser
-void parseArg(int argc, char const *argv[], struct raw_parameters params)
+raw_parameters parseArg(int argc, char const *argv[])
 {
+	raw_parameters params;
+	params.file = params.interface = params.syslogServer = params.time = "";
 	int option;
 
 	while ((option = getopt(argc, (char**)argv, "r:i:s:t:")) != -1) {
 		switch (option) {
 		case 'r':
-			if (params.file.compare("")) {
+			if (params.file.compare(""))
 				ERR_RET(help);
-			}
+
 			params.file = string(optarg);
 			break;
 		case 'i':
-			if (params.interface.compare("")) {
+			if (params.interface.compare(""))
 				ERR_RET(help);
-			}
+
 			params.interface = string(optarg);
 			break;
 		case 's':
-			if (params.syslogServer.compare("")) {
+			if (params.syslogServer.compare(""))
 				ERR_RET(help);
-			}
+
 			params.syslogServer = string(optarg);
 			break;
 		case 't':
-			if (params.time.compare("")) {
+			if (params.time.compare(""))
 				ERR_RET(help);
-			}
+
 			params.time = string(optarg);
 			break;
 		default:
 			ERR_RET(help);
 		}
 	}
+
+	return params;
 }
+
+/*********************************************************** INTERFACE ***************************************************************/
+
+void logInterface(string strInterface, string strLog, string strTime)
+{
+	int time;
+	if (strTime.compare(""))
+		time = 60;			// Default value
+	else
+	{
+		// Check for valid time
+		if (strTime.find_first_not_of("0123456789") == string::npos)
+        	time = stoi(strTime, NULL, 10);
+    	else
+        	ERR_RET("Time must be number in seconds!");
+	}
+
+
+
+}
+
+/*********************************************************** PCAP FILE ***************************************************************/
+
+void logFile(string strFile, string strLog)
+{
+	char errBuf[PCAP_ERRBUF_SIZE];
+	pcap_t* pcapFile;
+	if ((pcapFile = pcap_open_offline(strFile.c_str(), errBuf)) == NULL)
+		ERR_RET("Unable to open pcap file. Terminating.");
+
+	
+}
+
 /*********************************************************** MAIN ***************************************************************/
 
 // Main function
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     // Handle SIUSR1 
     signal(SIGUSR1, handleSig);
 
 	// Parse arguments
-	struct raw_parameters params;
-	parseArg(argc, argv, params);
+	raw_parameters params = parseArg(argc, argv);
 
+	if (params.interface.compare(""))
+	{
+		logInterface(params.interface, params.syslogServer, params.time);
+	}
+
+	if (params.file.compare(""))
+	{
+		logFile(params.file, params.syslogServer);
+	}
 
 
 	return 0;
