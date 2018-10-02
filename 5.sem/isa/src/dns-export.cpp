cc0834f952589dd7a197a530e10dec7a2f7149ab
tondo8881 <xnerec00@vutbr.cz>
2018-10-02 23:12:18 +0200

    Simple handler added.

--- 5.sem/isa/src/dns-export.cpp
+++ 5.sem/isa/src/dns-export.cpp
@@ -2,6 +2,14 @@
 #include <unistd.h>		// Getopt
 #include <signal.h>     // Signal shandling
 #include <pcap.h>		// No comment needed...
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <net/if.h>
+#include <netinet/if_ether.h>
+#include <net/ethernet.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
 
 using namespace std;
 
@@ -96,11 +104,36 @@ void logInterface(string strInterface, string strLog, string strTime)
         	ERR_RET("Time must be number in seconds!");
 	}
 
+	pcap_t* pcapInterface;
+	//if ((pcapInterface = pcap_open_live(strInterface.c_str(), 1, 1000, 1000))
+
 
 
 }
 
 /*********************************************************** PCAP FILE ***************************************************************/
+
+// This method is called in pcap_loop for each packet
+void pcapHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet)
+{
+	const struct ip* ipHeader;
+	char sourceIP[INET_ADDRSTRLEN];
+    char destIP[INET_ADDRSTRLEN];
+	u_int sourcePort, destPort;
+
+	const struct ether_header* ethernetHeader = (struct ether_header*)packet;
+    if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_IP) 
+	{
+
+        ipHeader = (struct ip*)(packet + sizeof(struct ether_header));
+        inet_ntop(AF_INET, &(ipHeader->ip_src), sourceIP, INET_ADDRSTRLEN);
+        inet_ntop(AF_INET, &(ipHeader->ip_dst), destIP, INET_ADDRSTRLEN);
+
+        
+	}
+
+}
+
 
 void logFile(string strFile, string strLog)
 {
@@ -109,8 +142,11 @@ void logFile(string strFile, string strLog)
 	if ((pcapFile = pcap_open_offline(strFile.c_str(), errBuf)) == NULL)
 		ERR_RET("Unable to open pcap file. Terminating.");
 
-	
+	if (pcap_loop(pcapFile, 0, pcapHandler, NULL) < 0)
+		ERR_RET("Error occured when reading pcap file. Terminating.");
+
 }
+
 
 /*********************************************************** MAIN ***************************************************************/
 
